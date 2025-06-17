a5e909183057851f90837acac1890a167f2d19a1
GoldFirePaw <camille.flajollet@gmail.com>
2025-06-17 15:11:35 +0200

    add logs in api calls

--- frontend/src/api/games/getGames.ts
+++ frontend/src/api/games/getGames.ts
@@ -1,15 +1,23 @@
-export const getGames = async () => {
+import type { Game } from "../../types";
+
+export const getGames = async (): Promise<Game[]> => {
   const API = import.meta.env.VITE_API_BASE_URL;
+  console.log("📡 Appel API:", `${API}/games`);
 
   try {
     const response = await fetch(`${API}/games`);
+    console.log("↩️ Réponse brute:", response);
+
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error("Erreur serveur");
     }
+
     const data = await response.json();
+    console.log("✅ Données reçues:", data);
+
     return data;
   } catch (error) {
-    console.error("Error fetching games:", error);
+    console.error("❌ Erreur getGames:", error);
     return [];
   }
 };
