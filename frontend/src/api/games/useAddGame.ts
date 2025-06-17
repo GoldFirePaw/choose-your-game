f79438741f86f6bf3adc327aa152ad646c18baa8
GoldFirePaw <camille.flajollet@gmail.com>
2025-06-17 14:02:51 +0200

    fix import

--- frontend/src/api/games/useAddGame.ts
+++ frontend/src/api/games/useAddGame.ts
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import { addGame } from '../api/addGame';
-import type { Game } from '../../types';
+import { useState } from "react";
+import type { Game } from "../../types";
+import { addGame } from "./addGame";
 
 export const useAddGame = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const submitGame = async (game: Omit<Game, 'id'>): Promise<Game | null> => {
+  const submitGame = async (game: Omit<Game, "id">): Promise<Game | null> => {
     setLoading(true);
     setError(null);
 
     const result = await addGame(game);
 
     if (!result) {
-      setError('Impossible d’ajouter le jeu.');
+      setError("Impossible d’ajouter le jeu.");
     }
 
     setLoading(false);
